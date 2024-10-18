5de0e0e5a41fb895998cb73ca0c93934fe5ff19a
Abdul Hafizh Firdaus <abdul.firdaus@astronauts.id>
2024-10-18 21:32:02 +0700

    chore: remove gorm tag

--- domain/history/entity_sql.go
+++ domain/history/entity_sql.go
@@ -3,14 +3,14 @@ package history
 import "time"
 
 type History struct {
-	ID           string    `db:"id" gorm:"primaryKey;column:id"`
-	Title        string    `db:"title" gorm:"column:title"`
-	URL          string    `db:"url" gorm:"column:url"`
-	UserID       string    `db:"user_id" gorm:"column:user_id"`
-	DeviceName   string    `db:"device_name" gorm:"column:device_name"`
-	LastActiveAt time.Time `db:"last_active_at" gorm:"column:last_active_at"`
-	CreatedAt    time.Time `db:"created_at" gorm:"column:created_at"`
-	UpdatedAt    time.Time `db:"updated_at" gorm:"column:updated_at"`
+	ID           string    `db:"id"`
+	Title        string    `db:"title"`
+	URL          string    `db:"url"`
+	UserID       string    `db:"user_id"`
+	DeviceName   string    `db:"device_name"`
+	LastActiveAt time.Time `db:"last_active_at"`
+	CreatedAt    time.Time `db:"created_at"`
+	UpdatedAt    time.Time `db:"updated_at"`
 }
 
 func (History) TableName() string {
